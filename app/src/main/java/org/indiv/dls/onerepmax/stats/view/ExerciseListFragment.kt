25027bd5e3149359ee8a7ba4c7069e649fa4ad89
Diana Sutlief <dsutlief@axon.com>
2021-08-21 15:19:34 -0700

    Renaming exercise list fragment, styling list items

--- app/src/main/java/org/indiv/dls/onerepmax/stats/view/ExerciseListFragment.kt
+++ app/src/main/java/org/indiv/dls/onerepmax/stats/view/ExerciseListFragment.kt
@@ -7,27 +7,26 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import org.indiv.dls.onerepmax.R
-import org.indiv.dls.onerepmax.databinding.FragmentExerciseBinding
+import org.indiv.dls.onerepmax.databinding.FragmentExerciseListBinding
 import org.indiv.dls.onerepmax.stats.viewmodel.ExercisesViewModel
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ExercisesFragment : Fragment() {
+class ExerciseListFragment : Fragment() {
 
     @Inject lateinit var exerciseListAdapter: ExerciseListAdapter
 
     private val exercisesViewModel: ExercisesViewModel by activityViewModels()
 
-    private var _binding: FragmentExerciseBinding? = null
+    private var _binding: FragmentExerciseListBinding? = null
 
     // This property is only valid between onCreateView and onDestroyView
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentExerciseBinding.inflate(inflater, container, false)
+        _binding = FragmentExerciseListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -37,7 +36,7 @@ class ExercisesFragment : Fragment() {
         exerciseListAdapter.itemClickListener = { exerciseName ->
             exercisesViewModel.selectSingleExerciseData(exerciseName)
             val args = Bundle().apply { putString(resources.getString(R.string.key_exercise_name), exerciseName) }
-            findNavController().navigate(R.id.action_ExerciseFragment_to_ExerciseDetailFragment, args)
+            findNavController().navigate(R.id.action_ExerciseListFragment_to_ExerciseDetailFragment, args)
         }
 
         binding.exerciseRecyclerView.adapter = exerciseListAdapter
